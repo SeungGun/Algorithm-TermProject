43ea99aae429ae21bbd751d3f6ef3236bb2ccd3f
SeungGun <saverrw2864@naver.com>
2020-11-22 16:17:44 +0900

    please

--- Algorithm_project/src/first/First.java
+++ Algorithm_project/src/first/First.java
@@ -3,6 +3,7 @@ package first;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -12,14 +13,13 @@ class UserInfo {
 	private int age;
 	private double height;
 	private double weight;
-	
+
 	double enoughSodium;
 	double enoughCalorie;
-	
-	double maximumSodium=3000;
+
+	double maximumSodium = 3000;
 	double maximumCalorie;
-	double maxiumumCholesterol=300;
-	
+	double maxiumumCholesterol = 300;
 
 	public UserInfo(String gender_, int age_, double height_, double weight_) {
 		this.gender = gender_;
@@ -27,15 +27,19 @@ class UserInfo {
 		this.height = height_;
 		this.weight = weight_;
 	}
+
 	public void setEnoughSodium(double a) {
-		this.enoughSodium=a;
+		this.enoughSodium = a;
 	}
+
 	public void setEnoughCalorie(double a) {
-		this.enoughCalorie=a;
+		this.enoughCalorie = a;
 	}
+
 	public void setMaximumCalorie(double a) {
-		this.maximumCalorie=a;
+		this.maximumCalorie = a;
 	}
+
 	public String getGender() {
 		return this.gender;
 	}
@@ -60,12 +64,12 @@ class FoodInfo {
 	double cholesterol;
 	int category;
 
-	public FoodInfo(String name_, double calories_, double sodium_, double cholesterol_,int category_) {
+	public FoodInfo(String name_, double calories_, double sodium_, double cholesterol_, int category_) {
 		this.name = name_;
 		this.calories = calories_;
 		this.sodium = sodium_;
 		this.cholesterol = cholesterol_;
-		this.category=category_;
+		this.category = category_;
 	}
 
 	public String getName() {
@@ -83,40 +87,47 @@ class FoodInfo {
 	public double getCholesterol() {
 		return this.cholesterol;
 	}
-}
-class DailyDiet{
-	String Breakfast[]=new String[3];
-	String Lunch[]=new String[3];
-	String Dinner[]=new String[3];
-	
-	double calorieTotal=0;
-	double sodiumTotal=0;
-	double cholesterolTotal=0;
-	
 
-	
-	boolean isjunkFood=false;
-	
+	public int getCategory() {
+		return this.category;
+	}
+}
+
+class DailyDiet {
+	String Breakfast[] = new String[3];
+	String Lunch[] = new String[3];
+	String Dinner[] = new String[3];
+
+	double calorieTotal = 0;
+	double sodiumTotal = 0;
+	double cholesterolTotal = 0;
+
+	boolean isjunkFood = false;
+
 	public void updateCalorie(double newCal) {
-		this.calorieTotal=this.calorieTotal+newCal;
+		this.calorieTotal = this.calorieTotal + newCal;
 	}
+
 	public void updateSodium(double newSod) {
-		this.sodiumTotal=this.sodiumTotal+newSod;
+		this.sodiumTotal = this.sodiumTotal + newSod;
 	}
+
 	public void updateCholesterol(double newCho) {
-		this.cholesterolTotal=this.cholesterolTotal+newCho;
+		this.cholesterolTotal = this.cholesterolTotal + newCho;
 	}
-	
-	
+
 	public double getCalorieDay() {
 		return this.calorieTotal;
 	}
+
 	public double getSodiumDay() {
 		return this.sodiumTotal;
 	}
+
 	public double getCholesterolDay() {
 		return this.cholesterolTotal;
 	}
+
 	public boolean getIsJunkFood() {
 		return this.isjunkFood;
 	}
@@ -126,18 +137,26 @@ public class First {
 	public static double Base = 0.0;
 	public static int Promotion = 0;
 	public static int num_food;
-	public static ArrayList<FoodInfo> ctg_junk;			//0
-	public static ArrayList<FoodInfo> ctg_stew;			//1
-	public static ArrayList<FoodInfo> ctg_meat;			//2
-	public static ArrayList<FoodInfo> ctg_drink;		//3
-	public static ArrayList<FoodInfo> ctg_agricultural;	//4
-	public static ArrayList<FoodInfo> ctg_snack;		//5
-	public static ArrayList<FoodInfo> ctg_other;		//6
+	public static ArrayList<FoodInfo> ctg_junk; // 0
+	public static ArrayList<FoodInfo> ctg_stew; // 1
+	public static ArrayList<FoodInfo> ctg_meat; // 2
+	public static ArrayList<FoodInfo> ctg_drink; // 3
+	public static ArrayList<FoodInfo> ctg_agricultural; // 4
+	public static ArrayList<FoodInfo> ctg_snack; // 5
+	public static ArrayList<FoodInfo> ctg_other; // 6
 	public static double[][] dp;
 	public static FoodInfo[] foodList;
 	public static HashSet<Integer> index_set;
+	public static HashSet<Double> values;
 	public static double sum = 0.0;
 	public static double remain_sodium = 0.0;
+	public static int tmp_index1 = 0;
+	public static int tmp_index2 = 0;
+	public static final int prefer_sodium = 3000;
+	public static ArrayList<Integer> backtracked;
+	public static boolean isThereJunk = false;
+	public static boolean[] isContaining; 
+	
 	public static void main(String[] args) {
 		String fileName = "foodInfo.txt";
 		Scanner inputStream = null;
@@ -152,16 +171,24 @@ public class First {
 			System.exit(0);
 		}
 		num_food = Integer.parseInt(inputStream.nextLine());
-		System.out.println("food# :"+num_food);
-		DailyDiet monday = new DailyDiet(); 
-		DailyDiet tuesday = new DailyDiet(); 
-		DailyDiet wednesday = new DailyDiet(); 
-		DailyDiet thursday = new DailyDiet(); 
-		DailyDiet friday = new DailyDiet(); 
-		DailyDiet saturday = new DailyDiet(); 
-		DailyDiet sunday = new DailyDiet(); 
-	
+		System.out.println("food count :" + num_food);
+
+		backtracked = new ArrayList<Integer>();
+
+		isContaining = new boolean[prefer_sodium+1];
+		Arrays.fill(isContaining, false);
+		
+		DailyDiet monday = new DailyDiet();
+		DailyDiet tuesday = new DailyDiet();
+		DailyDiet wednesday = new DailyDiet();
+		DailyDiet thursday = new DailyDiet();
+		DailyDiet friday = new DailyDiet();
+		DailyDiet saturday = new DailyDiet();
+		DailyDiet sunday = new DailyDiet();
+
+		
 		index_set = new HashSet<Integer>(num_food);
+		values = new HashSet<Double>(num_food);
 		
 		ctg_junk = new ArrayList<FoodInfo>();
 		ctg_stew = new ArrayList<FoodInfo>();
@@ -169,38 +196,38 @@ public class First {
 		ctg_drink = new ArrayList<FoodInfo>();
 		ctg_agricultural = new ArrayList<FoodInfo>();
 		ctg_snack = new ArrayList<FoodInfo>();
-		ctg_other= new ArrayList<FoodInfo>();
-		
+		ctg_other = new ArrayList<FoodInfo>();
+
 		Base = getBase(user);
 		Promotion = getPromotion(user);
-		
 		foodList = new FoodInfo[num_food];
-		dp = new double[num_food+1][3001];
-		
-		for(int i =0; i<num_food+1; i++) {
-			for(int j =0; j<3001; j++) {
+		dp = new double[num_food + 1][3001];
+
+		for (int i = 0; i < num_food + 1; i++) {
+			for (int j = 0; j < 3001; j++) {
 				dp[i][j] = -1;
 			}
 		}
-		// NAME | CALORIES | SODIUM | CHOLESTEROL
+		// NAME | CALORIES | SODIUM | CHOLESTEROL | CATEGORY
 		while (inputStream.hasNextLine()) {
 			String temp = inputStream.nextLine();
 			System.out.println(temp);
 			foodList[index] = new FoodInfo(temp.split(" ")[0], Double.parseDouble(temp.split(" ")[1]),
-					Double.parseDouble(temp.split(" ")[2]), Double.parseDouble(temp.split(" ")[3]),Integer.parseInt(temp.split(" ")[4]));
+					Double.parseDouble(temp.split(" ")[2]), Double.parseDouble(temp.split(" ")[3]),
+					Integer.parseInt(temp.split(" ")[4]));
 			index++;
 		}
-		user.enoughSodium=getSodiumBase(user);
-		user.enoughCalorie=getBase(user);
-		user.maximumCalorie=getPromotion(user);
+		user.enoughSodium = getSodiumBase(user);
+		user.enoughCalorie = getBase(user);
+		user.maximumCalorie = getPromotion(user);
 		System.out.println("\n");
-		System.out.println("You should eat " + user.enoughCalorie +" between "+user.maximumCalorie+" calorie");
+		System.out.println("You should eat " + user.enoughCalorie + " between " + user.maximumCalorie + " calorie");
 		System.out.println("\n");
-		System.out.println("You should eat " + user.enoughSodium +" between "+user.maximumSodium+" sodium");
+		System.out.println("You should eat " + user.enoughSodium + " between " + user.maximumSodium + " sodium");
 		System.out.println("\n");
 		System.out.println("You should eat cholesterol below " + user.maxiumumCholesterol);
-		
-		for(int i =0; i<foodList.length; i++) {
+
+		for (int i = 0; i < foodList.length; i++) {
 			sum += foodList[i].getCalories();
 		}
 		System.out.println(sum);
@@ -209,49 +236,79 @@ public class First {
 		}
 		System.out.println("\n");
 		System.out.println("Junk Foods Are: ");
-		for(int i=0; i<ctg_junk.size(); i++) {
+		for (int i = 0; i < ctg_junk.size(); i++) {
 			System.out.println(ctg_junk.get(i).getName());
 		}
 		System.out.println("\n");
 		System.out.println("Stews Are: ");
-		for(int i=0; i<ctg_stew.size(); i++) {
+		for (int i = 0; i < ctg_stew.size(); i++) {
 			System.out.println(ctg_stew.get(i).getName());
 		}
 		System.out.println("\n");
 		System.out.println("Meats Are: ");
-		for(int i=0; i<ctg_meat.size(); i++) {
+		for (int i = 0; i < ctg_meat.size(); i++) {
 			System.out.println(ctg_meat.get(i).getName());
 		}
 		System.out.println("\n");
 		System.out.println("Drinks Are: ");
-		for(int i=0; i<ctg_drink.size(); i++) {
+		for (int i = 0; i < ctg_drink.size(); i++) {
 			System.out.println(ctg_drink.get(i).getName());
 		}
 		System.out.println("\n");
 		System.out.println("Agricultural Are: ");
-		for(int i=0; i<ctg_agricultural.size(); i++) {
+		for (int i = 0; i < ctg_agricultural.size(); i++) {
 			System.out.println(ctg_agricultural.get(i).getName());
 		}
 		System.out.println("\n");
 		System.out.println("Snacks Are: ");
-		for(int i=0; i<ctg_snack.size(); i++) {
+		for (int i = 0; i < ctg_snack.size(); i++) {
 			System.out.println(ctg_snack.get(i).getName());
 		}
 		System.out.println("\n");
 		System.out.println("Others Are: ");
-		for(int i=0; i<ctg_other.size(); i++) {
+		for (int i = 0; i < ctg_other.size(); i++) {
 			System.out.println(ctg_other.get(i).getName());
 		}
-		
-		System.out.println(knapsack(0,3000));
-		
-		
+
+		System.out.println("Total Calories : " + knapsack(0, 3000) + " dp value : " + dp[tmp_index1][tmp_index2]
+				+ " Id1 , Id2 : " + tmp_index1 + " " + tmp_index2);
+
 		Iterator<Integer> iter = index_set.iterator();
-		while(iter.hasNext()) {
-			System.out.println(iter.next());
+		double ss = 0;
+		while (iter.hasNext()) {
+			ss += foodList[iter.next()].getCalories();
+
+			// System.out.println(" DWDWWDWD : "+iter.next());
 		}
-		System.out.println("remain sodium : "+remain_sodium);
+		System.out.println("iterator sum : " + ss);
+		System.out.println("remain sodium : " + remain_sodium);
+
+		for (int i = 0; i < num_food + 1; i++) {
+			for (int j = 0; j < prefer_sodium + 1; j++) {
+				if (dp[i][j] != -1)
+					System.out.print((int) dp[i][j] + " ");
+			}
+			System.out.println();
+		}
+		backtrackingDP(Promotion);
+		double s = 0;
+		for (int i = 0; i < backtracked.size(); i++) {
+			s += foodList[backtracked.get(i)].getCalories();
+			/*
+			 * if(s > Promotion) { s-= foodList[backtracked.get(i)].getCalories(); break; }
+			 */System.out.println("Name : " + foodList[backtracked.get(i)].getName() + " : "
+						+ foodList[backtracked.get(i)].getCalories());
+			System.out.println("s!! : " + s);
+			
+		}
+		System.out.println("backtracked sum : " + s);
+		findThreshold(index_set, s);
+		Iterator<Double> itt = values.iterator();
+		while(itt.hasNext()) {
+			System.out.println("values : "+itt.next());
+		}
 	}
+
 	public static double getBase(UserInfo u) {
 		double result = 0.0;
 		if (u.getGender().equals("M") || u.getGender().equals("m")) {
@@ -261,49 +318,101 @@ public class First {
 		}
 		return result;
 	}
-	
-	public static double knapsack(int num, int capacity) {
-		if(num == num_food) {
+
+	public static void findThreshold(HashSet<Integer> set, double sum) {
+		if(sum <= Promotion) return;
+		Iterator<Integer> it = set.iterator();
+		double max = -1;
+		int idx = -1;
+		double k = 0;
+		while (it.hasNext()) {
+			int to = it.next();
+			k = foodList[to].getCalories();
+			if (max < sum - k && sum - k <= Promotion) {
+				max = sum - k;
+				idx = to;
+			}
+		}
+		if(idx != -1)
+			set.remove(idx);
+		System.out.println("max : " + max + ", And extract : " + foodList[idx].getCalories() + " then index : " + idx);	
+		if(max > Promotion) {
+			findThreshold(set, max);
+		}
+
+	}
+
+	public static double knapsack(int num, double capacity) {
+		if (num == num_food) {
 			return 0;
 		}
-		double temp = dp[num][capacity];
-		if(temp != -1) return temp;
-		
-		if(foodList[num].getSodium() <= capacity) {
-			double tmp = knapsack(num+1, (int)(capacity - foodList[num].getSodium()));
-			temp = foodList[num].getCalories() + tmp;
-			if(temp > Promotion) {
-				temp = foodList[num].getCalories();
+		double temp = dp[num][(int)capacity];
+		if (temp != -1) 
+			return temp;		
+
+		if (foodList[num].getSodium() <= capacity) {
+			double tmp = knapsack(num + 1, (int) (capacity - foodList[num].getSodium()));
+			
+			if (foodList[num].getCategory() == 0 && !isThereJunk) {
+				isThereJunk = true;
+				System.out.println("leading to junk is : " + foodList[num].getName());
 			}
-			else {
-				remain_sodium = (int)(capacity - foodList[num].getSodium());
+			if (isThereJunk) {
+				if (foodList[num].getCategory() == 0)
+					temp = tmp;
+				else {
+					temp = foodList[num].getCalories() + tmp;
+					values.add(temp);
+				}
+			} else {
+				temp = foodList[num].getCalories() + tmp;
+				values.add(temp);
 			}
-			index_set.add(num);
+			if (temp > Promotion) {
+				temp = tmp;
+				tmp_index1 = num;
+				tmp_index2 = (int) capacity;
+
+			} else {
+				remain_sodium = (int) (capacity - foodList[num].getSodium());
+			}
+
 		}
-		
-		double tmp2 = knapsack(num+1,capacity);
-		if(tmp2 > temp && tmp2 != 0 && temp !=0) {
-			temp = tmp2; 
+
+		double tmp2 = knapsack(num + 1, capacity);
+		if (tmp2 > temp && tmp2 != 0 && temp != 0) {
+			temp = tmp2;
 		}
-		dp[num][capacity] = temp;
+		dp[num][(int)capacity] = temp;
 		return temp;
 	}
-	public static void backtrackingDP(int num, int capacity) {
-		
+
+	public static void backtrackingDP(int promotion) {
+		double t = dp[num_food][prefer_sodium];
+		for (int i = num_food; i >= 0; i--) {
+			for (int j = prefer_sodium; j >= 0; j--) {
+				if (dp[i][j] == -1)
+					continue;
+				if (t < dp[i][j]) {
+					t = dp[i][j];
+					System.out.println("t : " + t + ",, " + foodList[i].getCalories() + " i = , " + i + " j = , " + j
+							+ " Cal : " + foodList[i].getCalories() + " ," + foodList[i].getName());
+					backtracked.add(i);
+				}
+			}
+		}
 	}
 	public static int getPromotion(UserInfo u) {
 		int result = 0;
 		int a = u.getAge();
-		
-		if(a>=7 && a<=9) {
+
+		if (a >= 7 && a <= 9) {
 			result = 1800;
 			return result;
-		}
-		else if(a>=4 && a<=6) {
+		} else if (a >= 4 && a <= 6) {
 			result = 1600;
 			return result;
-		}
-		else if(a >=1 && a<=3) {
+		} else if (a >= 1 && a <= 3) {
 			result = 1200;
 			return result;
 		}
@@ -325,8 +434,7 @@ public class First {
 			} else if (a >= 10) {
 				result = 2200;
 			}
-		} 
-		else if (u.getGender().equals("W") || u.getGender().equals("w")) {
+		} else if (u.getGender().equals("W") || u.getGender().equals("w")) {
 			if (a >= 75) {
 				result = 1600;
 			} else if (a >= 65) {
@@ -343,38 +451,36 @@ public class First {
 				result = 2100;
 			} else if (a >= 10) {
 				result = 2000;
-			} 
+			}
 		}
 		return result;
 	}
+
 	public static int getSodiumBase(UserInfo u) {
 		int result = 0;
 		int a = u.getAge();
-	
-			if(a<=2) {
-				result= 900;
-			}else if (a<=5) {
-				result=1000;
-			}
-			else if (a<=8) {
-				result=1200;
-			}
-			else if (a<=11) {
-				result=1400;
-			}
-			else if(a<=64) {
-				result=1500;
-			}
-			else if(a<=74) {
-				result=1300;
-			}else {
-				result=1100;
-			}
-	
+
+		if (a <= 2) {
+			result = 900;
+		} else if (a <= 5) {
+			result = 1000;
+		} else if (a <= 8) {
+			result = 1200;
+		} else if (a <= 11) {
+			result = 1400;
+		} else if (a <= 64) {
+			result = 1500;
+		} else if (a <= 74) {
+			result = 1300;
+		} else {
+			result = 1100;
+		}
+
 		return result;
 	}
+
 	public static void foodSort(FoodInfo f) {
-		switch(f.category) {
+		switch (f.category) {
 		case 0:
 			ctg_junk.add(f);
 			break;
@@ -397,19 +503,21 @@ public class First {
 			ctg_other.add(f);
 		}
 	}
-	//public static boolean isJunkFood(FoodInfo f) {
-	//	if(f.getName().indexOf("pizza") >= 0 || f.getName().indexOf("Pizza") >= 0) {
-	//		return true;
-	//	}
-	//	else if(f.getName().indexOf("hamburger") >=0 || f.getName().indexOf("Hamburger") >= 0) {
-	//		return true;
-	//	}
-	//	else if(f.getName().indexOf("chicken")>=0 || f.getName().indexOf("Chicken") >= 0) {
-	//		return true;
-	//	}
-	//	else if(f.getCalories() < f.getSodium() && f.getCholesterol() > 10.0) {
-	//		return true;
-	//	}
-	//	return false;
-	//}
+	// public static boolean isJunkFood(FoodInfo f) {
+	// if(f.getName().indexOf("pizza") >= 0 || f.getName().indexOf("Pizza") >= 0) {
+	// return true;
+	// }
+	// else if(f.getName().indexOf("hamburger") >=0 ||
+	// f.getName().indexOf("Hamburger") >= 0) {
+	// return true;
+	// }
+	// else if(f.getName().indexOf("chicken")>=0 || f.getName().indexOf("Chicken")
+	// >= 0) {
+	// return true;
+	// }
+	// else if(f.getCalories() < f.getSodium() && f.getCholesterol() > 10.0) {
+	// return true;
+	// }
+	// return false;
+	// }
 }
